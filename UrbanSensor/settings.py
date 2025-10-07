21dc8db697972677bc5e57588353c63fa09850c4
Binyamin8914 <benjamin.munoz8914@gmail.com>
2025-10-06 23:00:36 -0300

    Cambios en la app core para el dashboard

--- UrbanSensor/settings.py
+++ UrbanSensor/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'heroes',
     'administracion',
     'incidencias',
 ]
@@ -124,7 +123,7 @@ LOGOUT_REDIRECT_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
